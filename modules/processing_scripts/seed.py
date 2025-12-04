a0535a14dd293b2b39560809f899acacfdf77f5d
BasharKarndal2 <basharkarndal20013@gmail.com>
2025-12-04 01:39:54 -0800

    add translation to UI

--- modules/processing_scripts/seed.py
+++ modules/processing_scripts/seed.py
@@ -31,25 +31,25 @@ class ScriptSeed(scripts.ScriptBuiltinUI):
     def ui(self, is_img2img):
         with gr.Row(elem_id=self.elem_id("seed_row")):
             if cmd_opts.use_textbox_seed:
-                self.seed = gr.Textbox(label='Seed', value="", elem_id=self.elem_id("seed"), min_width=100)
+                self.seed = gr.Textbox(label='البذرة', value="", elem_id=self.elem_id("seed"), min_width=100)
             else:
-                self.seed = gr.Number(label='Seed', value=-1, elem_id=self.elem_id("seed"), min_width=100, precision=0)
+                self.seed = gr.Number(label='البذرة', value=-1, elem_id=self.elem_id("seed"), min_width=100, precision=0)
 
             random_seed = ToolButton(ui.random_symbol, elem_id=self.elem_id("random_seed"), tooltip="Set seed to -1, which will cause a new random number to be used every time")
             reuse_seed = ToolButton(ui.reuse_symbol, elem_id=self.elem_id("reuse_seed"), tooltip="Reuse seed from last generation, mostly useful if it was randomized")
 
-            seed_checkbox = gr.Checkbox(label='Extra', elem_id=self.elem_id("subseed_show"), value=False)
+            seed_checkbox = gr.Checkbox(label='توسيع', elem_id=self.elem_id("subseed_show"), value=False)
 
         with gr.Group(visible=False, elem_id=self.elem_id("seed_extras")) as seed_extras:
             with gr.Row(elem_id=self.elem_id("subseed_row")):
-                subseed = gr.Number(label='Variation seed', value=-1, elem_id=self.elem_id("subseed"), precision=0)
+                subseed = gr.Number(label='تباين البذرة', value=-1, elem_id=self.elem_id("subseed"), precision=0)
                 random_subseed = ToolButton(ui.random_symbol, elem_id=self.elem_id("random_subseed"))
                 reuse_subseed = ToolButton(ui.reuse_symbol, elem_id=self.elem_id("reuse_subseed"))
-                subseed_strength = gr.Slider(label='Variation strength', value=0.0, minimum=0, maximum=1, step=0.01, elem_id=self.elem_id("subseed_strength"))
+                subseed_strength = gr.Slider(label='قوة البذرة', value=0.0, minimum=0, maximum=1, step=0.01, elem_id=self.elem_id("subseed_strength"))
 
             with gr.Row(elem_id=self.elem_id("seed_resize_from_row")):
-                seed_resize_from_w = gr.Slider(minimum=0, maximum=2048, step=8, label="Resize seed from width", value=0, elem_id=self.elem_id("seed_resize_from_w"))
-                seed_resize_from_h = gr.Slider(minimum=0, maximum=2048, step=8, label="Resize seed from height", value=0, elem_id=self.elem_id("seed_resize_from_h"))
+                seed_resize_from_w = gr.Slider(minimum=0, maximum=2048, step=8, label="إعادة تحجيم البذرة من العرض", value=0, elem_id=self.elem_id("seed_resize_from_w"))
+                seed_resize_from_h = gr.Slider(minimum=0, maximum=2048, step=8, label=" إعادة تحجيم البذرة من الطول ", value=0, elem_id=self.elem_id("seed_resize_from_h"))
 
         random_seed.click(fn=None, _js="function(){setRandomSeed('" + self.elem_id("seed") + "')}", show_progress=False, inputs=[], outputs=[])
         random_subseed.click(fn=None, _js="function(){setRandomSeed('" + self.elem_id("subseed") + "')}", show_progress=False, inputs=[], outputs=[])
