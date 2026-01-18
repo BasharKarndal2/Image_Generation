a6a0a72fc207180a02b1cff8b1704f5bc5549f8b
BasharKarndal2 <basharkarndal20013@gmail.com>
2026-01-18 07:09:37 -0800

    Clear Python and command line settings in batch filell
    Removed specific Python executable and command line arguments.

--- webui-user.bat
+++ webui-user.bat
@@ -1,7 +1,7 @@
 @echo off
 
-set PYTHON=C:\Users\Bashar\AppData\Local\Programs\Python\Python310\python.exe
-set COMMANDLINE_ARGS=--skip-torch-cuda-test --use-cpu all --no-half --no-half-vae --precision full
+set PYTHON=
+set COMMANDLINE_ARGS=
 set VENV_DIR=
 
 call webui.bat
